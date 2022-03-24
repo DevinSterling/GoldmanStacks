fb963507ae5678cf42042f445cead40f318b67d6
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-03-24 15:20:01 -0400

    Update home.php

--- web/home.php
+++ web/home.php
@@ -35,7 +35,7 @@ $amountOfTransactions = 5; // Number of recent transactions to show
 	<body>
 		<nav class="menubar">
 			<ul class="menugroup">
-				<li class="menulogo"><a href="home.php">TempBank</a></li>
+				<li class="menulogo"><a href="home.php">Goldman Stacks</a></li>
                 		<li class="menutoggle"><a href="#"><i class="fas fa-bars"></i></a></li>
 				<li class="menuitem"><a href="home.php">Home</a></li>
 				<li class="menuitem"><a href="account/transfer.php">Transfer</a></li>
