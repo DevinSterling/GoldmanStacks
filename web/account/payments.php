68aaeabf53ad0883e6b0eaeb79928b0934760602
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-03-24 15:25:08 -0400

    Update payments.php
    Updated logo text

--- web/account/payments.php
+++ web/account/payments.php
@@ -31,7 +31,7 @@ $amountOfPayments = 5;
 	<body>
 	<nav class="menubar">
 		<ul class="menugroup">
-			<li class="menulogo"><a href="../home.php">TempBank</a></li>
+			<li class="menulogo"><a href="../home.php">Goldman Stacks</a></li>
 			<li class="menutoggle"><a href="#"><i class="fas fa-bars"></i></a></li>
 			<li class="menuitem"><a href="../home.php">Home</a></li>
 			<li class="menuitem"><a href="transfer.php">Transfer</a></li>
