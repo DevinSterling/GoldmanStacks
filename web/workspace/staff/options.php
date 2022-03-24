0a2eccf3d74ed03adab71ba75547786b2b2e8cfb
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-03-24 14:52:53 -0400

    Update options.php
    Updated paths to relative

--- web/workspace/staff/options.php
+++ web/workspace/staff/options.php
@@ -10,12 +10,12 @@ $lastLog = date("F j, Y, g:i a"); // Last time of log
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
-		<title>Options</title>
-		<!-- Stylesheet -->
-		<link rel="stylesheet" href="/~sterlid2/bank/CSS/stylesheet.css">
-		<!-- Favicon -->
-		<link rel="icon" href="/~sterlid2/bank/Images/logo.ico">
-		<!-- Google Font -->
+	<title>Options</title>
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="../../CSS/stylesheet.css">
+	<!-- Favicon -->
+	<link rel="icon" href="../../Images/logo.ico">
+	<!-- Google Font -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <!-- Google Font -->
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
