01d6de186c84d85a54ebf8f62a072de72ee8f2ba
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 01:23:27 -0400

    Create config.php

---
+++ private/config.php
@@ -0,0 +1,56 @@
+<?
+// Database
+const HOST = "";
+const DATABASE = "";
+
+// Database Accounts
+const QUERY_USERNAME = ""; // Used when querying rows
+const QUERY_PASSWORD = "";
+const INSERT_USERNAME = ""; // Used when inserting rows
+const INSERT_PASSWORD = "";
+const UPDATE_USERNAME = ""; // Used when updating rows
+const UPDATE_PASSWORD = "";
+const DELETE_USERNAME = ""; // Used when deleting rows
+const DELETE_PASSWORD = "";
+
+// Database Connections
+function getQueryConnection() {
+    $conn = new mysqli(HOST, QUERY_USERNAME, QUERY_PASSWORD, DATABASE);
+    
+    if (mysqli_connect_errno()) { // If true, db connection failed
+        return null;
+    }
+    
+    return $conn;
+}
+
+function getInsertConnection() {
+    $conn = new mysqli(HOST, INSERT_USERNAME, INSERT_PASSWORD, DATABASE);
+    
+    if (mysqli_connect_errno()) { // If true, db connection failed
+        return null;
+    }
+    
+    return $conn;
+}
+
+function getUpdateConnection() {
+    $conn = new mysqli(HOST, UPDATE_USERNAME, UPDATE_PASSWORD, DATABASE);
+    
+    if (mysqli_connect_errno()) { // If true, db connection failed
+        return null;
+    }
+    
+    return $conn;
+}
+
+function getDeleteConnection() {
+    $conn = new mysqli(HOST, DELETE_USERNAME, DELETE_PASSWORD, DATABASE);
+    
+    if (mysqli_connect_errno()) { // If true, db connection failed
+        return null;
+    }
+    
+    return $conn;
+}
+?>
