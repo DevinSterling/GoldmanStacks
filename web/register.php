8d11e38a16931fc7882ff7fd76e5300a2b207502
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 18:25:52 -0400

    Added fetch API

--- web/register.php
+++ web/register.php
@@ -58,64 +58,96 @@ $registrationToken = hash_hmac('sha256', '/authenticateRegistration.php', $_SESS
                         <h2 class="big"><b>Goldman Stacks</b><!-- <span class="info">Registration</span>--><h2>
                     </div>
                     <br>
-                    <form id="edit">
-                        <label for="email" class="info">Email</label>
-        	            <div class="form-item">
-        		            <input id="email" name="email" type="email" class="input-field" required>
-        	            </div>
-        	            <label for="password" class="info">Password</label>
-        	            <div class="form-item">
-        		            <input id="password" name="password" type="password" class="input-field"required>
-        	            </div>
-        	            <label for="confirm-password" class="info">Confirm Password</label>
-        	            <div class="form-item">
-        		            <input id="confirm-password" name="confirm-password" type="password" class="input-field" required>
-        	            </div>
-        	            <hr>
-                        <label for="phone-number" class="info">Phone Number</label>
-        	            <div class="form-item">
-        		            <input id="phone-number" name="phone-number" type="text" pattern="1?\d{3}-?\d{3}-?{4}" class="input-field" required>
-        	            </div>
-        	            <label for="ssn" class="info">SSN</label>
-        	            <div class="form-item">
-        		            <input id="ssn" name="ssn" type="text" pattern="^\d{3}-?\d{2}-?\d{4}$" class="input-field" required>
-        	            </div>
-        	            <hr>
-                        <label for="address-line1" class="info">Address Line 1</label>
-        	            <div class="form-item">
-        		            <input id="address-line1" name="address-line1" type="text" pattern="^\d+ [A-z ]+.?$" class="input-field" required>
-        	            </div>
-        	            <label for="address-line2" class="info">Address Line 2</label>
-        	            <div class="form-item">
-        		            <input id="address-line2" name="address-line2" type="text" pattern="^[A-z0-9#, ]+$" class="input-field">
-        	            </div>
-        	            <label for="address-city" class="info">City</label>
-        	            <div class="form-item">
-        		            <input id="address-city" name="address-city" type="text" pattern="^[A-z. ]+$" class="input-field" required>
-        	            </div>
-        	            <label for="address-state" class="info">State</label>
-        	            <div class="form-item">
-        		            <input id="address-state" name="address-state" type="text" pattern="^[A-z ]+$" class="input-field" required>
-        	            </div>
-        	            <label for="address-postal-code" class="info">Postal Code</label>
-        	            <div class="form-item">
-        		            <input id="address-postal-code" name="address-postal-code" type="text" pattern="^[0-9]{5}$" class="input-field" required>
-        	            </div>
-        	            <hr>
+                    <form id="register">
+			    <label for="email" class="info">Email</label>
+			    <div class="form-item">
+				    <input id="email" name="email" type="email" class="input-field" required>
+			    </div>
+			    <label for="password" class="info">Password</label>
+			    <div class="form-item">
+				    <input id="password" name="password" type="password" class="input-field"required>
+			    </div>
+			    <label for="confirm-password" class="info">Confirm Password</label>
+			    <div class="form-item">
+				    <input id="confirm-password" name="confirm-password" type="password" class="input-field" required>
+			    </div>
+			    <hr>
+			    <label for="phone-number" class="info">Phone Number</label>
+			    <div class="form-item">
+				    <input id="phone-number" name="phone-number" type="text" pattern="1?\d{3}-?\d{3}-?{4}" class="input-field" required>
+			    </div>
+			    <label for="ssn" class="info">SSN</label>
+			    <div class="form-item">
+				    <input id="ssn" name="ssn" type="text" pattern="^\d{3}-?\d{2}-?\d{4}$" class="input-field" required>
+			    </div>
+			    <hr>
+			    <label for="address-line1" class="info">Address Line 1</label>
+			    <div class="form-item">
+				    <input id="address-line1" name="address-line1" type="text" pattern="^\d+ [A-z ]+.?$" class="input-field" required>
+			    </div>
+			    <label for="address-line2" class="info">Address Line 2</label>
+			    <div class="form-item">
+				    <input id="address-line2" name="address-line2" type="text" pattern="^[A-z0-9#, ]+$" class="input-field">
+			    </div>
+			    <label for="address-city" class="info">City</label>
+			    <div class="form-item">
+				    <input id="address-city" name="address-city" type="text" pattern="^[A-z. ]+$" class="input-field" required>
+			    </div>
+			    <label for="address-state" class="info">State</label>
+			    <div class="form-item">
+				    <input id="address-state" name="address-state" type="text" pattern="^[A-z ]+$" class="input-field" required>
+			    </div>
+			    <label for="address-postal-code" class="info">Postal Code</label>
+			    <div class="form-item">
+				    <input id="address-postal-code" name="address-postal-code" type="text" pattern="^[0-9]{5}$" class="input-field" required>
+			    </div>
+			    <hr>
 			    <input type="hidden" name="token" value="<? echo $registrationToken ?>">
-                        <div class="form-item">
-                            <button form="edit" class="standard-button transform-button flex-center round">
-                                <div class="split">
-                                    <p class="animate-left">Request Registration<p>
-                   		        <div class="toggle-button">
-                    		    <i class="fas fa-chevron-right"></i>
-                    		</div>
-                                </div>
-                            </button>
-                        </div>
+			    <div class="form-item">
+				<button type="submit" class="standard-button transform-button flex-center round">
+				    <div class="split">
+					<p class="animate-left">Request Registration<p>
+					<div class="toggle-button">
+					    <i class="fas fa-chevron-right"></i>
+					</div>
+				    </div>
+				</button>
+			</div>
                     </form>
                 </div>
             </div>
         </div>
     </body>
+    <script type="text/javascript" src="Scripts/notification.js"></script>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', () => {
+            document.getElementById('register').addEventListener('submit', handleForm);
+        });
+        
+        function handleForm(event) {
+            event.preventDefault();
+	        
+            let form = event.target;
+            let formData = new FormData(form);
+	        
+            let url = "requests/register.php";
+            let request = new Request(url, {
+	        body: formData,
+	        method: 'POST',
+            });
+	        
+	    fetch(request)
+	        .then((response) => response.json())
+	        .then((data) => {
+		    showNotification();
+
+	            if (data.response) {
+	                setSuccessNotification(data.message);
+	            } else {
+	                setFailNotification(data.message);
+	            }
+	        })
+	        .catch(console.warn);
+    }
+    </script>
 </html>
