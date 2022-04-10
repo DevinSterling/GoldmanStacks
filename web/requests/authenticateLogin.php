89026d2c507d64c3b14b2ea6aef2f25179eaa651
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 01:10:05 -0400

    Create authenticateLogin.php

---
+++ web/requests/authenticateLogin.php
@@ -0,0 +1,70 @@
+<?
+require_once('/home/sterlid2/Private/config.php');
+require_once('/home/sterlid2/Private/userbase.php');
+
+/* Force https connection */
+forceHTTPS();
+
+/* Check if the user is logged in already */
+session_start();
+if(checkIfLoggedIn()) {
+    die();
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+/* Defaults */
+$dbSuccess = false;
+$dbFailMessage = "Invalid Credentials Provided";
+$dbMessage = "";
+
+if (!(empty($username) || empty($password))) { // if true, non-empty parameters given and passwords match
+    /* DB Connection */
+    $db = getUpdateConnection();
+    
+    if ($db !== null) {
+        /* Verify Current Password */
+        $query = $db->prepare("SELECT userID, userRole, password FROM users WHERE email=?");
+        $query->bind_param("s", $username);
+        $query->execute();
+        $result = $query->get_result();
+        
+        /* Check if user exists */
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            
+            /* Compare passwords */
+            if ($password === $user['password']) {
+                /* Start Session */
+                session_start();
+                session_regenerate_id(true);
+                
+                $_SESSION['uid'] = $user['userID']; // Set User Id for Session
+                $_SESSION['role'] = $user['userRole']; // Set User Role for Session
+                $_SESSION['key'] = bin2hex(random_bytes(32)); // Create Session Key for CSRF tokens
+                
+                $dbSuccess = true;
+            } else {
+                $dbMessage = $dbFailMessage;
+            }
+        } else {
+            $dbMessage = $dbFailMessage;
+        }
+        
+        /* Close Streams */
+        $result->free();
+        $query->close();
+        $db->close();
+    } else {
+        $dbMessage = "Cannot Connect To Database";
+    }
+}
+
+/* Return Outcome */
+$myObj = (object)array();
+$myObj->response = $dbSuccess;
+$myObj->message = $dbMessage;
+$myJSON = json_encode($myObj);
+echo $myJSON;
+?>
