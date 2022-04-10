05301a36c26cad30c65dc6848ea6068bc7d86356
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 12:04:18 -0400

    Update userbase.php

--- private/userbase.php
+++ private/userbase.php
@@ -1,4 +1,5 @@
 <?
+// Forces https connection
 function forceHTTPS() {
     if($_SERVER["HTTPS"] != "on") {
         header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
@@ -10,6 +11,16 @@ function forceHTTPS() {
 function checkIfLoggedIn() {
     if (isset($_SESSION['uid']) && isset($_SESSION['role'])) return true;
     return false;
+}
+
+// Inactivity Detection
+function checkInactive() {
+    if ($_SESSION['last_activity'] < time() - $_SESSION['expire_time'] ) { // Inactive User Condition
+        return true;
+    } else {
+        $_SESSION['last_activity'] = time();
+        return false;
+    }
 }
 
 // Check if the current user is a client
