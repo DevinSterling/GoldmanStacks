1b2e4865d54abbdcf23b4445bcd0f7a799ab550e
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 21:51:50 -0400

    Update authenticateRegistration.php

--- web/requests/authenticateRegistration.php
+++ web/requests/authenticateRegistration.php
@@ -93,10 +93,11 @@ if (hash_equals($calc, $token) // Check token
                 $insertClient->execute();
               
                 $clientId = $insertClient->insert_id;
-              
-                /*$insertAddress = $db->prepare("INSERT INTO address VALUES (?, ?, ?, ?, ?) WHERE userID=?");
+                $insertClient->close();
+                
+                $insertAddress = $db->prepare("INSERT INTO address VALUES (?, ?, ?, ?, ?) WHERE userID=?");
                 $insertAddress->bind_param("sssssi", $addressLine1, $addressLine2, $addressCity, $addressState, $addressPostalCode, $clientId);
-                $insertAddress->execute();*/
+                $insertAddress->execute();
 
                 /* Check Execution */
                 if ($db->affected_rows === 0) {
