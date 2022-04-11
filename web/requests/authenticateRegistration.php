4542161709354353eaed97c5c2ccd9e32aa7c84a
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 20:22:09 -0400

    Create authenticateRegistration.php

---
+++ web/requests/authenticateRegistration.php
@@ -0,0 +1,131 @@
+<?
+require_once('/home/sterlid2/Private/config.php');
+require_once('/home/sterlid2/Private/userbase.php');
+
+/* Force https connection */
+forceHTTPS();
+
+/* Check if the user is logged in already */
+session_start();
+if(checkIfLoggedIn()) {
+    die();
+}
+
+/* POST Variables */
+/* Name related info */
+$firstName = $_POST['first-name'];
+$middleName = $_POST['middle-name'];
+$lastName = $_POST['last-name']
+  ;
+/* Security related info */
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirmPassword = $_POST['confirm-password'];
+$phoneNumber = $_POST['phone-number'];
+$ssn = $_POST['ssn'];
+
+/* Address related info */
+$addressLine1 = $_POST['address-line1'];
+$addressLine2 = $_POST['address-line2'];
+$addressCity = $_POST['address-city'];
+$addressState = $_POST['address-state'];
+$addressPostalCode = $_POST['address-postal-code'];
+
+/* Token */
+$token = $_POST['token'];
+
+/* Defaults */
+$dbSuccess = false;
+$dbMessage = "";
+
+$dbFailMessage = "Failed to register account";
+
+/* Confirm token and parameters */
+$calc = hash_hmac('sha256', '/authenticateRegistration.php', $_SESSION['key']);
+if (hash_equals($calc, $token) // Check token
+    && !(empty($firstName) // Check for empty parameters
+    || empty($middleName)
+    || empty($lastName)
+    || empty($email)
+    || empty($password)
+    || empty($phoneNumber)
+    || empty($ssn)
+    || empty($addressLine1)
+    || empty($addressCity)
+    || empty($addressState)
+    || empty($addressPostalCode))
+    && ($password === $confirmPassword)) { // Check if given passwords match
+    
+    $phoneNumber = str_replace('-', '', $phoneNumber); // Remove hyphens if provided by user
+  
+    /* Input Validation */
+    $isMatch = (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
+    $isMatch &= preg_match('/^\d{10}$/', $phoneNumber); // Check if phone number matches requirement
+    $isMatch &= preg_match('/^\d{3}-?\d{2}-?\d{4}$/', $ssn);
+    $isMatch &= preg_match('/^\d+ [A-z ]+.?$/', $addressLine1);
+    $isMatch &= preg_match('/^[A-z. ]+$/', $addressCity);
+    $isMatch &= preg_match('/^[A-z ]+$/', $addressState);
+    $isMatch &= preg_match('/^[0-9]{5}$/', $addressPostalCode);
+  
+    if (!empty($addressLine2)) {
+        $isMatch &= preg_match('/^[A-z0-9#, ]+$/', $addressLine2);
+    }
+  
+    if ($isMatch) {
+        /* DB Connection */
+        $db = getUpdateConnection();
+      
+        if ($db !== null) {
+            $queryEmail = $db->prepare("SELECT email FROM users WHERE email=?");
+            $queryEmail->bind_param("s", $email);
+            $queryEmail->execute();
+            $result = $queryEmail->get_result();
+            
+            /* Verify if email is not registered yet */
+            if ($result->num_rows > 0) {
+                /* Verify Current Password */
+                $insertClient = $db->prepare("INSERT INTO users (userRole, email, password, firstName, middleName, lastName, lastName, ssn) VALUES ('client', ?, ?, ?, ?, ?, ?, ?)");
+                $insertClient->bind_param("sssssss", $email, $password, $firstName, $middleName, $lastName, $ssn);
+                $insertClient->execute();
+              
+                $clientId = $insertClient->insert_id;
+              
+                $insertAddress = $db->prepare("INSERT INTO address VALUES (?, ?, ?, ?, ?) WHERE userID=?");
+                $insertAddress->bind_param("sssssi", $addressLine1, $addressLine2, $addressCity, $addressState, $addressPostalCode, $clientId);
+                $insertAddress->execute();
+
+                /* Check Execution */
+                if ($db->affected_rows === 0) {
+                    $dbSuccess = true;
+                    $dbMessage = "Account has been registered";
+                } else {
+                    $dbMessage = $dbFailMessage+"C";
+                }
+              
+                $insertClient->close();
+                $insertAddress->close();
+            } else {
+                $dbMessage = "Provided email is registered already";
+            }
+
+            /* Close Streams */
+            $result->free();
+            $queryEmail->close();
+            $db->close();
+        } else {
+            $dbMessage = $dbFailMessage+"B";
+        }
+    } else {
+        $dbMessage = $dbFailMessage+"A";
+    }
+} else {
+    die();
+}
+
+/* Return Outcome */
+$myObj = (object)array();
+$myObj->response = $dbSuccess;
+$myObj->message = $dbMessage;
+$myJSON = json_encode($myObj);
+echo $myJSON;
+?>
