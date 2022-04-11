0f1f04d554b69adcb0e4d8f9e6dd37ea66727962
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-10 20:45:27 -0400

    Update authenticateRegistration.php

--- web/requests/authenticateRegistration.php
+++ web/requests/authenticateRegistration.php
@@ -120,7 +120,7 @@ if (hash_equals($calc, $token) // Check token
         $dbMessage = $dbFailMessage+"A";
     }
 } else {
-    $dbMessage = $firstName+$middleName+$lastName;
+    $dbMessage = $firstName;
 }
 
 /* Return Outcome */
