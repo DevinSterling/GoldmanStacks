391e604757866828c20bebb2e1cdd5d716366d49
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-12 21:29:30 -0400

    Added new queries

--- public_html/goldmanstacks/requests/authenticateRegistration.php
+++ public_html/goldmanstacks/requests/authenticateRegistration.php
@@ -14,6 +14,7 @@ if (checkIfLoggedIn()) die();
 $firstName = $_POST['first-name'];
 $middleName = $_POST['middle-name'];
 $lastName = $_POST['last-name'];
+$birthDate = $_POST['birth-date'];
 
 /* Security related info */
 $email = $_POST['email'];
@@ -43,23 +44,25 @@ $calc = hash_hmac('sha256', '/authenticateRegistration.php', $_SESSION['key']);
 
 /* Confirm token and user input */
 if (hash_equals($calc, $token) // Check token
-    && checkNotEmpty($firstName, $lastName, $email, $password, $phoneNumber, $ssn, $addressLine1, $addressCity, $addressState, $addressPostalCode) // Check for empty parameters
+    && checkNotEmpty($firstName, $lastName, $birthDate, $email, $password, $phoneNumber, $ssn, $addressLine1, $addressCity, $addressState, $addressPostalCode) // Check for empty parameters
     && ($password === $confirmPassword)) { // Check if given passwords match
     
     /* String Manipulation */
     $phoneNumber = str_replace('-', '', $phoneNumber); // Remove hyphens if provided by user
     $ssn = str_replace('-', '', $ssn); // Remove hyphens if provided by user
-  
+    $birthDate = date("Y-m-d", strtotime($birthDate)); // Convert HTML date to SQL compatible date
+
     /* Input Validation */
     $isMatch = (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
     $isMatch &= ctype_alpha($firstName);
     $isMatch &= ctype_alpha($lastName);
-    $isMatch &= preg_match('/^\d{10}$/', $phoneNumber); // Check if phone number matches requirement
+    $isMatch &= preg_match('/^\d{10}$/', $phoneNumber);
     $isMatch &= preg_match('/^\d{9}$/', $ssn);
     $isMatch &= preg_match('/^\d+ [A-z ]+.?$/', $addressLine1);
     $isMatch &= preg_match('/^[A-z. ]+$/', $addressCity);
-    $isMatch &= preg_match('/^[A-z ]+$/', $addressState);
+    $isMatch &= preg_match('/^[A-Z]{2}$/', $addressState);
     $isMatch &= preg_match('/^[0-9]{5}$/', $addressPostalCode);
+    $isMatch &= (bool)strtotime($birthDate);
     
     /* Optional Input Validation */
     if (!empty($middleName)) $isMatch &= ctype_alpha($middleName);
@@ -72,6 +75,7 @@ if (hash_equals($calc, $token) // Check token
         $db = getUpdateConnection();
       
         if ($db !== null) {
+            /* Query to search if email is registered already */
             $queryEmail = $db->prepare("SELECT email FROM users WHERE email=?");
             $queryEmail->bind_param("s", $email);
             $queryEmail->execute();
@@ -79,30 +83,48 @@ if (hash_equals($calc, $token) // Check token
             
             /* Verify if email is not registered yet */
             if ($result->num_rows === 0) {
-                /* Encrypt provided password */
-                $password = password_hash($password, PASSWORD_DEFAULT);
+                /* Query to search if phone number is registered already */
+                $queryPhoneNumber = $db->prepare("SELECT phoneNumber FROM users WHERE phoneNumber=?");
+                $queryPhoneNumber->bind_param("s", $phoneNumber);
+                $queryPhoneNumber->execute();
+                $result = $queryPhoneNumber->get_result();
                 
-                /* Verify Current Password */
-                $insertClient = $db->prepare("INSERT INTO users (userRole, email, password, firstName, middleName, lastName, phoneNumber, ssn) VALUES ('client', ?, ?, ?, ?, ?, ?, ?)");
-                $insertClient->bind_param("sssssss", $email, $password, $firstName, $middleName, $lastName, $phoneNumber, $ssn);
-                $insertClient->execute();
-              
-                $clientId = $insertClient->insert_id;
-                $insertClient->close();
-                
-                $insertAddress = $db->prepare("INSERT INTO address VALUES (?, ?, ?, ?, ?, ?)");
-                $insertAddress->bind_param("isssss", $clientId, $addressLine1, $addressLine2, $addressCity, $addressState, $addressPostalCode);
-                $insertAddress->execute();
-
-                /* Check Execution */
-                if ($db->affected_rows > 0) {
-                    $dbSuccess = true;
-                    $dbMessage = "Account has been registered";
+                /* Verify if phone number is not registered yet */
+                if ($result->num_rows === 0) {
+                    /* Encrypt provided password */
+                    $password = password_hash($password, PASSWORD_DEFAULT);
+                    
+                    /* Insert user into database */
+                    $insertUser = $db->prepare("INSERT INTO users (userRole, email, password, firstName, middleName, lastName, phoneNumber, ssn) VALUES ('client', ?, ?, ?, ?, ?, ?, ?)");
+                    $insertUser->bind_param("sssssss", $email, $password, $firstName, $middleName, $lastName, $phoneNumber, $ssn);
+                    $insertUser->execute();
+                  
+                    $clientId = $insertUser->insert_id;
+                    $insertUser->close();
+                    
+                    /* Insert associated client information */
+                    $insertClient = $db->prepare("INSERT INTO client VALUES (?, 0, ?)");
+                    $insertClient->bind_param("is", $clientId, $birthDate);
+                    $insertClient->execute();
+                    $insertClient->close();
+                    
+                    /* Insert associated address information */
+                    $insertAddress = $db->prepare("INSERT INTO address VALUES (?, ?, ?, ?, ?, ?)");
+                    $insertAddress->bind_param("isssss", $clientId, $addressLine1, $addressLine2, $addressCity, $addressState, $addressPostalCode);
+                    $insertAddress->execute();
+    
+                    /* Check Execution */
+                    if ($db->affected_rows > 0) {
+                        $dbSuccess = true;
+                        $dbMessage = "Account has been registered";
+                    } else {
+                        $dbMessage = $db->error;
+                    }
+                  
+                    $insertAddress->close();
                 } else {
-                    $dbMessage = $dbFailMessage;
+                    $dbMessage = "Provided phone number is registered already";
                 }
-              
-                $insertAddress->close();
             } else {
                 $dbMessage = "Provided email is registered already";
             }
