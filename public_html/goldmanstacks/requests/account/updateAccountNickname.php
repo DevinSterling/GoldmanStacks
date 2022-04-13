9fe4f8092ec9aeb5ff2847707d449d76db622238
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-13 15:57:15 -0400

    Added database functionality

--- public_html/goldmanstacks/requests/account/updateAccountNickname.php
+++ public_html/goldmanstacks/requests/account/updateAccountNickname.php
@@ -1,2 +1,67 @@
 <?php
-?>
+require_once('../../../../private/config.php');
+require_once('../../../../private/userbase.php');
+
+forceHTTPS(); // Force https connection
+session_start(); // Start Session
+checkClientStatus(); // Check if the client is signed in
+
+/* SESSION Variables */
+$userID = $_SESSION["uid"];
+
+/* POST Variables */
+$accountNickName = $_POST['new'];
+$currentNickName = $_POST['old'];
+$token = $_POST['token'];
+
+/* Defaults */
+$dbSuccess = false;
+$dbMessage = "";
+
+$dbFailMessage = "Failed to update nickname";
+
+/* Calculate expected token */
+$calc = hash_hmac('sha256', '/updateAccountNickname.php', $_SESSION['key']); 
+
+/* Confirm token and user input */
+if (hash_equals($calc, $token)
+    && !empty($accountNickName)) {
+    
+    /* Input Validation */
+    $isMatch = preg_match('/^[A-z0-9 ]+$/', $accountNickName);
+    
+    if ($isMatch) {
+        /* DB Connection */
+        $db = getUpdateConnection();
+        
+        if ($db !== null) {
+            /* Check if the account type has been requested by the user already */
+            $updateNickName = $db->prepare("UPDATE accountDirectory SET nickName=? WHERE nickName=? AND clientID=?");
+            $updateNickName->bind_param("ssi", $accountNickName, $currentNickName, $userID);
+            $updateNickName->execute();
+            
+            if ($db->affected_rows > 0) {
+                $dbSuccess = true;
+                $dbMessage = "Nickname has been updated";
+            } else {
+                $dbMessage = $dbFailMessage;
+            }
+            
+            $updateNickName->close();
+            $db->close();
+        } else {
+            $dbMessage = $dbFailMessage;
+        }
+    } else {
+        $dbMessage = $dbFailMessage;
+    }
+} else {
+    die();
+}
+    
+$object = (object)array();
+$object->response = $dbSuccess;
+$object->message = $dbMessage;
+$json = json_encode($object);
+
+echo $json;
