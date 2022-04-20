13fd7bd96ae9b9d2659a8d72faaf192fa8f48f3d
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-19 22:02:19 -0400

    Added Fetch API

--- public_html/goldmanstacks/js/post.js
+++ public_html/goldmanstacks/js/post.js
@@ -1,25 +1,28 @@
-// Source: https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
-// useage: post('/path/file', {var:"123"})
-
-function post(path, params, method='post') {
-
-  // The rest of this code assumes you are not using a library.
-  // It can be made less verbose if you use one.
-  const form = document.createElement('form');
-  form.method = method;
-  form.action = path;
-
-  for (const key in params) {
-    if (params.hasOwnProperty(key)) {
-      const hiddenField = document.createElement('input');
-      hiddenField.type = 'hidden';
-      hiddenField.name = key;
-      hiddenField.value = params[key];
-
-      form.appendChild(hiddenField);
+async function getJson(url, formData) {
+    /* Json to retrieve */
+    let json = null;
+    
+    /* Create request */
+    request = new Request(url, {
+        body: formData,
+        method: 'POST',
+    });
+    
+    /* Retrieve response */
+    const response = await fetch(request).catch((error) => {
+        /* Returns null on network error */
+        return null;
+    });
+    
+    /* Check response status code */
+    if (response.status >= 200 && response.status <= 299) {
+        /* Retrieve json */
+        json = response.json();
     }
-  }
+    
+    return json;
+}
 
-  document.body.appendChild(form);
-  form.submit();
+function isEmptyJson(json) {
+    return json === null;
 }
