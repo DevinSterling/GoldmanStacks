35cba93c56192835a821f98024b646cd3bf752c4
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-27 16:16:49 -0400

    Added partial database connectivity

--- public_html/goldmanstacks/view/workspace/manage/user.php
+++ public_html/goldmanstacks/view/workspace/manage/user.php
@@ -1,11 +1,27 @@
 <?php
 require_once('../../../../../private/sysNotification.php');
 require_once('../../../../../private/userbase.php');
+require_once('../../../../../private/config.php');
+require_once('../../../../../private/userbase.php');
+
+forceHTTPS(); // Force https connection
+session_start(); // Start Session
+//checkClientStatus(); // Check if the client is signed in
+
+/* GET Variables */
+$user = $_GET['id'];
+
+$db = getUpdateConnection();
+
+if ($db === null) {
+    header("Location: ");
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
-	<title>User</title>
+	<title>Manage User</title>
 	<!-- Stylesheet -->
 	<link rel="stylesheet" href="../../../css/stylesheet.css">
 	<!-- Favicon -->
@@ -36,10 +52,132 @@ require_once('../../../../../private/userbase.php');
 		</nav>
 		<div class="sys-notification">Logged as Employee</div>
 		<?php notification(); ?>
-	    <div class="list main">
-	        <div class="container">
-    	        <h2 id="title"><?php echo $currentAccountName?> User (id)</h2>
-    	    </div>
+    	<div class="container flex-center">
+            <div class="list sub">
+    	        <div class="container">
+        	        <h2 id="title"><?php echo $currentAccountName?> User <?php echo $user ?></h2>
+                    <?php
+                        /* Query */
+                        $queryUser = $db->prepare("SELECT firstName, middleName, lastName, email, phoneNumber, line1, line2, city, state, postalCode FROM users U INNER JOIN address A ON U.userID=A.userID WHERE U.userID=?");
+                        $queryUser->bind_param("i", $user);
+                        $queryUser->execute();
+                        
+                        /* Result */
+                        $resultUser = $queryUser->get_result();
+                        $user = $resultUser->fetch_assoc();
+                    ?>
+                    <h5 class="big-info">User Information</h5>
+                    <p class="info"><b>First Name</b>: <?php echo htmlspecialchars($user['firstName']) ?></p>
+                    <p class="info"><b>Last Name</b>: <?php echo htmlspecialchars($user['lastName']) ?></p>
+                    <hr>
+                    <h5 class="big-info">Contact Information</h5>
+                    <p class="info"><b>Email Address</b>: <?php echo htmlspecialchars($user['email']) ?></p>
+                    <p class="info"><b>Phone Number</b>: <?php echo htmlspecialchars($user['phoneNumber']) ?></p>
+                    <hr>
+                    <h5 class="big-info">Address Information</h5>
+                    <p class="info"><b>Line 1</b>: <?php echo htmlspecialchars($user['line1']) ?></p>
+                    <?php
+			        if (!empty($user['line2'])) echo "<p class=\"info\"><b>Line 2</b>:".htmlspecialchars($user['line2'])."</p>";
+                    ?>
+                    <p class="info"><b>City</b>: <?php echo htmlspecialchars($user['city']) ?></p>
+                    <p class="info"><b>State</b>: <?php echo htmlspecialchars($user['state']) ?></p>
+                    <p class="info"><b>Postal Code</b>: <?php echo htmlspecialchars($user['postalCode']) ?></p>
+                    <?php
+                        /* Release */
+                        $resultUser->free();
+                        $queryUser->close();
+                        $db->close(); 
+                    ?>
+        	    </div>
+            </div>
+            <div class="list fixed-sub">
+		        <div class="container fixed-sub fixed-item">
+		            <div class="margin-bottom">
+    		            <label class="banner-text">Account Actions</label>
+    		            <hr>
+    		            <button class="highlight-button transform-button split round">
+                            <div class="list">
+                                <p class="banner-text">Remove Account</p>
+                            </div>
+                            <div class="animate-left">
+                	            <div class="toggle-button">
+                	                <i class="fas fa-chevron-right"></i>
+                	            </div>
+                            </div>
+        		        </button>
+        		        <hr>
+    		            <button class="highlight-button transform-button split round">
+                            <div class="list">
+                                <p class="banner-text">Edit Account</p>
+                            </div>
+                            <div class="animate-left">
+                	            <div class="toggle-button">
+                	                <i class="fas fa-chevron-right"></i>
+                	            </div>
+                            </div>
+        		        </button>
+        		        <hr>
+    		            <button class="highlight-button transform-button split round">
+                            <div class="list">
+                                <p class="banner-text">View Transactions</p>
+                            </div>
+                            <div class="animate-left">
+                	            <div class="toggle-button">
+                	                <i class="fas fa-chevron-right"></i>
+                	            </div>
+                            </div>
+        		        </button>
+        		        <hr>
+    		            <button class="highlight-button transform-button split round">
+                            <div class="list">
+                                <p class="banner-text">View Payments</p>
+                            </div>
+                            <div class="animate-left">
+                	            <div class="toggle-button">
+                	                <i class="fas fa-chevron-right"></i>
+                	            </div>
+                            </div>
+        		        </button>
+    		        </div>
+		            <label class="banner-text">Other</label>
+		            <hr>
+		            <a href="open" class="highlight-button transform-button split round">
+                        <div class="list">
+                            <p class="banner-text">Manage Open Requests</p>
+                            <small>Bank Account Management</small>
+                        </div>
+                        <div class="animate-left">
+            	            <div class="toggle-button">
+            	                <i class="fas fa-chevron-right"></i>
+            	            </div>
+                        </div>
+    		        </a>
+    		        <hr>
+    		        <a href="../registration" class="highlight-button transform-button split round">
+                        <div class="list">
+                            <p class="banner-text">Manage Registration Requests</p>
+                            <small>Registration Management</small>
+                        </div>
+                        <div class="animate-left">
+            	            <div class="toggle-button">
+            	                <i class="fas fa-chevron-right"></i>
+            	            </div>
+                        </div>
+    		        </a>
+    		        <hr>
+    		        <a href="../user" class="highlight-button transform-button split round">
+                        <div class="list">
+                            <p class="banner-text">Manage Userbase</p>
+                            <small>User Management</small>
+                        </div>
+                        <div class="animate-left">
+            	            <div class="toggle-button">
+            	                <i class="fas fa-chevron-right"></i>
+            	            </div>
+                        </div>
+    		        </a>
+		        </div>
+		    </div>
         </div>
 	</body>
 </html>
