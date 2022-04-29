0ea7fc0f3b98f4e5f2ee2aa370c2d931b3119504
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-29 16:08:12 -0400

    Added partial database connectivity

--- public_html/goldmanstacks/requests/workspace/user/removeUser.php
+++ public_html/goldmanstacks/requests/workspace/user/removeUser.php
@@ -1,7 +1,52 @@
 <?php
+require_once('../../../../../private/config.php');
+require_once('../../../../../private/userbase.php');
+require_once('../../../../../private/functions.php');
+
+forceHTTPS(); // Force https connection
+session_start(); // Start Session
+checkEmployeeStatus(); // Check if the employee is signed in
+
+session_start();
+
+/* SESSION Variables */
+$key = $_SESSION['key'];
+
+/* POST Variables */
+$userID = $_POST['id'];
+$token = $_POST['token'];
+
+/* Object variables */
+$dbResponse = false;
+$dbMessage = 'Failed to remove user';
+
+/* Calculate expected token */
+$calc = hash_hmac('sha256', '/removeUser.php', $key);
+
+/* Confirm token and user input */
+if (hash_equals($calc, $token)
+    && !empty($userID)) {
+    
+    /* Input Validation */
+    $isMatch = is_numeric($userID);
+    
+    if ($isMatch) {
+        /* Get database connection */
+        $db = getUpdateConnection();
+         
+        /* Check connection */
+        if ($db !== null) {
+            $dbResponse = true;
+            $dbMessage = "Fetch API Success!";
+            
+            $db->close();
+        }
+    }
+}
+
 $object = (object)array();
-$object->response = true;
-$object->message = "Fetch API Success";
+$object->response = $dbResponse;
+$object->message = $dbMessage;
 
 $json = json_encode($object);
 
