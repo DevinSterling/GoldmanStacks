4367cfcbb21922d3513f4d7026ee61e5adbb56ad
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-29 16:14:30 -0400

    Rename and added partial database connectivity

---
+++ public_html/goldmanstacks/requests/workspace/account/approveCloseRequest.php
@@ -0,0 +1,53 @@
+<?php
+require_once('../../../../../private/config.php');
+require_once('../../../../../private/userbase.php');
+require_once('../../../../../private/functions.php');
+
+forceHTTPS(); // Force https connection
+session_start(); // Start Session
+checkEmployeeStatus(); // Check if the employee is signed in
+
+session_start();
+
+/* SESSION Variables */
+$key = $_SESSION['key'];
+
+/* POST Variables */
+$clientID = $_POST['id'];
+$token = $_POST['token'];
+
+/* Object variables */
+$dbResponse = false;
+$dbMessage = 'Failed to approve close request';
+
+/* Calculate expected token */
+$calc = hash_hmac('sha256', '/approveCloseRequest.php', $key);
+
+/* Confirm token and user input */
+if (hash_equals($calc, $token)
+    && !empty($clientID)) {
+    
+    /* Input Validation */
+    $isMatch = is_numeric($clientID);
+    
+    if ($isMatch) {
+        /* Get database connection */
+        $db = getUpdateConnection();
+         
+        /* Check connection */
+        if ($db !== null) {
+            $dbResponse = true;
+            $dbMessage = "Fetch API Success!";
+            
+            $db->close();
+        }
+    }
+}
+
+$object = (object)array();
+$object->response = $dbResponse;
+$object->message = $dbMessage;
+
+$json = json_encode($object);
+
+echo $json;
