cbf3f3b29fadb8ea63e24c883e9b506d8c3e1f9c
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-05-01 16:41:29 -0400

    Added confirmation and balance checking

--- public_html/goldmanstacks/view/account/funds.php
+++ public_html/goldmanstacks/view/account/funds.php
@@ -1,18 +1,57 @@
 <?php
 require_once('../../../../private/sysNotification.php');
+require_once('../../../../private/config.php');
+require_once('../../../../private/functions.php');
 require_once('../../../../private/userbase.php');
 
 forceHTTPS(); // Force https connection
 session_start(); // Start Session
 checkClientStatus(); // Check if the client is signed in
 
+/* SESSION Variables */
+$key = $_SESSION['key'];
+$userID = $_SESSION['uid'];
+
+/* GET Variables */
 $view = $_GET['v'];
 $referencedName = $_GET['acc'];
 
-/* Temp Variables*/
-$accounts = ["Checking", "Savings", "Account 3", "Account 4", "Account 5"]; // User Account names taken from DB
+/* Variables */
+$accounts = array();
+$isReferenced = false;
 
-$amountOfAccounts = 5;
+/* Csrf form tokens */
+$depositToken = hash_hmac('sha256', '/newDeposit.php', $key);
+$withdrawToken = hash_hmac('sha256', '/newWithdraw.php', $key);
+$getBalanceToken = hash_hmac('sha256', '/getBalance.php', $key);
+
+/* Get Database Connection */
+$db = getUpdateConnection();
+
+/* Check Database Connection */
+if ($db === null) {
+    header("Location: ../error/error.php");
+    die();
+}
+
+/* Get client accounts */
+$queryAccounts = $db->prepare("SELECT nickName, accountType, accountNum, balance FROM accountDirectory WHERE clientID=?");
+$queryAccounts->bind_param("i", $userID);
+$queryAccounts->execute();
+
+$resultAccounts = $queryAccounts->get_result();
+$rowAccounts = $resultAccounts->fetch_all(MYSQLI_ASSOC);
+
+foreach ($rowAccounts as $account) {
+    /* Create three dimensional associative array */
+    $accounts[] = array('nickName' => $account['nickName'], 'type' => $account['accountType'], 'number' => $account['accountNum'], 'balance' => $account['balance']);
+    
+    if ($referencedName === $account['nickName']) $isReferenced = true;
+}
+
+$resultAccounts->free();
+$queryAccounts->close();
+$db->close();
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -34,23 +73,31 @@ $amountOfAccounts = 5;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-	<nav class="menubar">
-		<ul class="menugroup">
-			<li class="menulogo"><a href="../home">Goldman Stacks</a></li>
-			<li class="menutoggle"><a href="#"><i class="fas fa-bars"></i></a></li>
-			<li class="menuitem"><a href="../home">Home</a></li>
-			<li class="menuitem"><a href="transfer">Transfer</a></li>
-			<li class="menuitem"><a href="payments">Payments</a></li>
-			<li class="menuitem"><a href="open">Open New Account</a></li>
-			<li class="menuitem"><a href="statement">Statement</a></li>
-		</ul>
-		<ul class="menugroup">
-			<li class="menuitem"><a href="../user/options">Options</a></li>
-			<li class="menuitem"><a href="../../requests/signout">Sign Out</a></li>
-		</ul>
-	</nav>
-	<?php 
-	notification();
+    	<nav class="menubar">
+    		<ul class="menugroup">
+    			<li class="menulogo"><a href="../home">Goldman Stacks</a></li>
+    			<li class="menutoggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+    			<li class="menuitem"><a href="../home">Home</a></li>
+    			<li class="menuitem"><a href="transfer">Transfer</a></li>
+    			<li class="menuitem"><a href="payments">Payments</a></li>
+    			<li class="menuitem"><a href="open">Open New Account</a></li>
+    			<li class="menuitem"><a href="statement">Statement</a></li>
+    		</ul>
+    		<ul class="menugroup">
+    			<li class="menuitem"><a href="../user/options">Options</a></li>
+    			<li class="menuitem"><a href="../../requests/signout">Sign Out</a></li>
+    		</ul>
+    	</nav>
+	    <?php notification() ?>
+    	<button id="notification" type="button" onClick="hideNotification()" class="notification sub success transform-button round collapse">
+            <p><i id="notification-icon" class="fas fa-check icon"></i><span id="notification-text"></span></p>
+            <div class="split">
+                   <div class="toggle-button">
+    	            <i class="fas fa-times"></i>
+    	        </div>
+            </div>
+        </button>
+	    <?php
         if (!empty($referencedName)) {
             echo "<div class=\"container flex-center marginless-bottom\">
                 <div class=\"list sub\">
@@ -101,19 +148,33 @@ $amountOfAccounts = 5;
                         else echo "Withdraw"
                     ?></h2>
                 </div>
-                <form id="deposit-form" class="flex-form <?php if (!empty($view) && $view !== "deposit") echo "hidden" ?>">
-                    <p class="info">Deposit funds from account</p><br>
+                <form id="deposit-form" action="../../requests/account/newDeposit" class="flex-form <?php if (!empty($view) && $view !== "deposit") echo "hidden" ?>">
+                    <p class="info">Deposit funds into account</p><br>
 		            <label for="deposit-account" class="info">Account</label>
-		            <select id="deposit-account" class="input-field">
+		            <select id="deposit-account" name="account" class="input-field" required>
                         <?php
-                        for ($i = 0; $i < $amountOfAccounts; $i++) {
-                            echo "<option>$accounts[$i]</option>";
+                        if (!$isReferenced) {
+                            $balance = $accounts[0]['balance'];
+                        }
+                        
+                        foreach ($accounts as $account) {
+                            echo "<option value=\"" . encrypt($account['number'], $key) . "\"";
+                           
+                            if ($referencedName === $account['nickName']) {
+                                $balance = $account['balance'];
+                                echo " selected";
+                            }
+                           
+                            echo ">" . ($account['nickName'] . " (" . ucfirst($account['type']) . ")" ) . "</option>";
                         }
                         ?>
 		            </select>
+		            <p class="info">Balance: $<span id="deposit-balance"><?php echo number_format($balance, 2) ?></span></p>
+		            <hr>
                     <label for="deposit-amount" class="info">Amount</label>
-                    <input id="deposit-amount" type="number" class="input-field">
-                    <button form="filterDate" class="standard-button transform-button flex-center round">
+                    <input id="deposit-amount" type="number" step="0.01" name="amount" class="input-field" required>
+                    <input type="hidden" name="token" value="<?php echo $depositToken ?>">
+                    <button type="submit" class="standard-button transform-button flex-center round">
                         <div class="split">
                             <p class="animate-left">Deposit<p>
            		            <div class="toggle-button">
@@ -122,19 +183,33 @@ $amountOfAccounts = 5;
                         </div>
                     </button>
                 </form>
-                <form id="withdraw-form" class="flex-form <?php if ($view !== "withdraw") echo "hidden" ?>">
+                <form id="withdraw-form" action="../../requests/account/newWithdraw" class="flex-form <?php if ($view !== "withdraw") echo "hidden" ?>">
                     <p class="info">Withdraw funds from account</p><br>
 		            <label for="withdraw-account" class="info">Account</label>
-		            <select id="withdraw-account" class="input-field">
+		            <select id="withdraw-account" name="account" class="input-field" required>
                         <?php
-                        for ($i = 0; $i < $amountOfAccounts; $i++) {
-                            echo "<option>$accounts[$i]</option>";
+                        if (!$isReferenced) {
+                            $balance = $accounts[0]['balance'];
+                        }
+                        
+                        foreach ($accounts as $account) {
+                            echo "<option value=\"" . encrypt($account['number'], $key) . "\"";
+                           
+                            if ($referencedName === $account['nickName']) {
+                                $balance = $account['balance'];
+                                echo " selected";
+                            }
+                           
+                            echo ">" . ($account['nickName'] . " (" . ucfirst($account['type']) . ")" ) . "</option>";
                         }
                         ?>
 		            </select>
+		            <p class="info">Balance: $<span id="withdraw-balance"><?php echo number_format($balance, 2) ?></span></p>
+                    <hr>
                     <label for="withdraw-amount" class="info">Amount</label>
-                    <input id="withdraw-amount" type="number" class="input-field">
-                    <button form="filterDate" class="standard-button transform-button flex-center round">
+                    <input id="withdraw-amount" type="number" step="0.01" name="amount" class="input-field" required>
+                    <input type="hidden" name="token" value="<?php echo $withdrawToken ?>">
+                    <button type="submit" class="standard-button transform-button flex-center round">
                         <div class="split">
                             <p class="animate-left">Withdraw<p>
            		            <div class="toggle-button">
@@ -147,7 +222,173 @@ $amountOfAccounts = 5;
     	    <div class="list mini">
     	    </div>
     	</div>
-	    <script type="text/javascript" src="../../js/navigation.js"></script>
-	    <script type="text/javascript" src="../../js/tabs.js"></script>
+        <div id="pop-up" class="pop-up">
+            <div onClick="hidePopUp()" class="flex-center-item">
+            </div>
+            <div id="pup-up-element" class="pop-up-content fixed-sub round hidden">
+                <button onClick="hidePopUp()" class="expand-button transform-button extend-right round">
+                    <div class="split">
+                        <p class="condensed-info"><i class="fas fa-arrow-left"></i></p>
+                        <div class="animate-right">
+        		            <div class="toggle-button">
+        		                <p class="expanded-info">Return</p>
+        		            </div>
+        	            </div>
+                    </div>
+                </button>
+                <br>
+                <br>
+                <div class="flex-form">
+                    <h2 id="title"><span id="transaction-type"></span> Confirmation</h2>
+                    <p class="info">Bank Account</p>
+                    <p id="transaction-account"></p>
+                    <p class="info">Amount</p>
+                    <p>$<span id="transaction-amount"></span></p>
+                    <button id="confirm-transaction" type="submit" class="standard-button transform-button flex-center round">
+                        <div class="split">
+                            <p class="animate-left">Confirm<p>
+           		            <div class="toggle-button">
+            		            <i class="fas fa-chevron-right"></i>
+            		        </div>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </div>
     </body>
+    <script type="text/javascript" src="../../js/navigation.js"></script>
+    <script type="text/javascript" src="../../js/notification.js"></script>
+    <script type="text/javascript" src="../../js/post.js"></script>
+    <script type="text/javascript" src="../../js/tabs.js"></script>
+	<script type="text/javascript">
+	    /* PopUp Contents */
+	    const transactionType = document.getElementById('transaction-type');
+	    const transactionAccount = document.getElementById('transaction-account');
+	    const transactionAmount = document.getElementById('transaction-amount');
+	    
+	    /* Form Contents */
+	    const depositAccount = document.getElementById('deposit-account');
+	    const depositBalance = document.getElementById('deposit-balance');
+	    const withdrawAccount = document.getElementById('withdraw-account');
+	    const withdrawBalance = document.getElementById('withdraw-balance');
+	    
+	    let form = null;
+	    let formData = null;
+	    let balance = null;
+	
+	    document.getElementById('deposit-form').addEventListener('submit', showPopUp);
+	    document.getElementById('withdraw-form').addEventListener('submit', showPopUp);
+	    document.getElementById('confirm-transaction').addEventListener('click', handleForm);
+	    depositAccount.addEventListener('change', async () => {
+	        await retrieveBalance(event.target.value);
+	        depositBalance.textContent = balance;
+	    });
+	    withdrawAccount.addEventListener('change', async () => {
+	        await retrieveBalance(event.target.value);
+	        withdrawBalance.textContent = balance;
+	    });
+	    
+        function showPopUp(event) {
+            event.preventDefault();
+            
+	        form = event.target;
+	        formData = new FormData(form);
+	        
+	        if (verifyUserInput()) {
+	            hideNotification(); // Hide notification if visible
+	            
+                transactionAmount.textContent = formData.get('amount');
+                
+                document.getElementById("pop-up").classList.add('show-popup-content');
+                document.getElementById("pup-up-element").classList.remove('hidden');
+	        } else {
+                showNotification();
+	        }
+        }
+        
+        function verifyUserInput() {
+            let verified = false;
+            
+	        if (form.id === 'deposit-form') {
+	            if (Number(formData.get('amount')) > Number(depositBalance.textContent.replace(',', ''))) {
+	                setFailNotification("Requested amount is over the current balance");
+	            } else {
+	                transactionType.textContent = 'Deposit';
+                    transactionAccount.textContent = depositAccount.selectedOptions[0].text;
+                    verified = true;
+	            }
+	        } else {
+                if (Number(formData.get('amount')) > Number(withdrawBalance.textContent.replace(',', ''))) {
+	                setFailNotification("Requested amount is over the current balance");
+	            } else {
+	                transactionType.textContent = 'Withdraw';
+                    transactionAccount.textContent = withdrawAccount.selectedOptions[0].text;
+                    verified = true;
+	            }
+	        }
+	        
+	        return verified;
+        }
+        
+        function hidePopUp() {
+            document.getElementById("pop-up").classList.remove("show-popup-content");
+            document.getElementById("pup-up-element").classList.add("hidden");
+        }
+	    
+	    async function handleForm(event) {
+	        let json = await getJson(form.action, formData);
+	        
+	        if (!isEmptyJson()) {
+	            if (json.response) {
+                    if (form.id === 'deposit-form') {
+                        setSuccessNotification('Deposited $' + formData.get('amount') + ' to ' + transactionAccount.textContent);
+                        depositAccount.dispatchEvent(new Event('change'));
+                    } else {
+                        setSuccessNotification('Withdrawed $' + formData.get('amount') + ' from ' + transactionAccount.textContent);
+                        withdrawAccount.dispatchEvent(new Event('change'));
+                    }
+                    
+                    form.reset();	            
+	            } else {
+	                setFailNotification(json.message);
+	            }
+	        } else {
+	            setFailNotification("Failed");
+	        }
+	        
+	        showNotification();
+	    }
+        
+	    async function retrieveBalance(value) {
+            let failure = true; // Used to notify user if something went wrong
+            
+            /* Create data to POST */
+	        let data = new FormData();
+	        data.append('account', value);
+	        data.append('token', '<?php echo $getBalanceToken ?>');
+	        
+            /* Retrieve associated json */
+	        let json = await getJson('../../requests/account/getBalance', data);
+	       
+	        /* Check if the given json is not empty*/
+	        if (!isEmptyJson(json)) {
+	            /* Check if computation done by server was successful */
+                if (json.response) {
+	                failure = false;
+                    balance = json.balance;
+	            } else {
+	                balance = '0.00';
+	                setFailNotification(json.message);
+	            }
+	        } else {
+	            setFailNotification('Failed to retrieve details');
+	        }
+	        
+	        if (failure) {
+    	        /* Notify user */
+    	        showNotification();
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+	        }
+	    }
+	</script>
 </html>
