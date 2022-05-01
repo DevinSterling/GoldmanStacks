d475746e343d4fed386464ecbcab59de6192074d
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-04-30 21:07:08 -0400

    Added database functionality

--- public_html/goldmanstacks/requests/account/payment/deletePayment.php
+++ public_html/goldmanstacks/requests/account/payment/deletePayment.php
@@ -16,11 +16,8 @@ $paymentID = decrypt($_POST['payment'], $key);
 $token = $_POST['token'];
 
 /* Database response */
-$response = (object)array();
-$response->response = false;
-$response->message = '';
-
-$dbFailMessage = 'Failed to remove payment';
+$dbSuccess = false;
+$dbMessage = 'Failed to remove payment';
 
 /* Calculate expected token */
 $calc = hash_hmac('sha256', '/deletePayment.php', $key);
@@ -30,24 +27,46 @@ if (hash_equals($calc, $token)
     && !empty($paymentID)) {
     
     /* Input validation code goes here */
-    
+    $isMatch = is_numeric($paymentID);
+
     if ($isMatch) {
         /* Get database connection */
         $db = getUpdateConnection();
         
         /* Check database connection */
         if ($db !== null) {
+            $selectStatement = $db->prepare("SELECT COUNT(*) 
+                                            FROM payments P INNER JOIN accountDirectory A ON P.accountNum=A.accountNum 
+                                            WHERE P.paymentID=? AND A.clientID=?");
+            $selectStatement->bind_param("si", $paymentID, $userID);
+            $selectStatement->execute();
+            $selectStatement->store_result();
             
-            /* Code goes here */
+            $selectStatement->bind_result($count);
+            $selectStatement->fetch();
+            $selectStatement->close();
+            
+            if ($count) {
+                $deleteStatement = $db->prepare("DELETE FROM payments WHERE paymentID=?");
+                $deleteStatement->bind_param("s", $paymentID);
+                $deleteStatement->execute();
+                
+                if ($db->affected_rows === 1) {
+                    $dbSuccess = true;
+                    $dbMessage = "Payment has been removed";
+                }
+                
+                $deleteStatement->close();
+            }
             
             $db->close();
-        } else {
-            $response->message = $dbFailMessage;
-        }
-    }
-} else {
-    $response->message = $dbFailMessage;
+        } else $dbMessage = "test";
+    } else $dbMessage = "test?";
 }
+
+$response = (object)array();
+$response->response = $dbSuccess;
+$response->message = $dbMessage;
 
 /* Return outcome */
 $json = json_encode($response);
