28fa2447b19e5fbc42e7cf38b48919f9eb2427cf
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-05-02 03:55:40 -0400

    Added partial database connectivity

--- public_html/goldmanstacks/view/workspace/staff/options.php
+++ public_html/goldmanstacks/view/workspace/staff/options.php
@@ -6,6 +6,8 @@ forceHTTPS(); // Force https connection
 session_start(); // Start Session
 checkEmployeeStatus(); // Check if the employee is signed in
 
+/* CSRF token */
+$passwordToken = hash_hmac('sha256', '/updatePassword.php', $key);
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -32,7 +34,6 @@ checkEmployeeStatus(); // Check if the employee is signed in
 				<li class="menulogo"><a href="../manager">Goldman Stacks</a></li>
                 		<li class="menutoggle"><a href="#"><i class="fas fa-bars"></i></a></li>
 				<li class="menuitem"><a href="../manager">Manage</a></li>
-				<li class="menuitem"><a href="../search">Search</a></li>
 			</ul>
 			<ul class="menugroup">
 				<li class="menuitem"><a href="options">Options</a></li>
@@ -41,6 +42,14 @@ checkEmployeeStatus(); // Check if the employee is signed in
 		</nav>
 		<div class="sys-notification">Logged as Employee</div>
 		<?php notification(); ?>
+        <button id="notification" onClick="hideNotification()" class="notification sub success transform-button round collapse">
+            <p><i id="notification-icon" class="fas fa-check icon"></i><span id="notification-text"></span></p>
+            <div class="split">
+   	            <div class="toggle-button">
+		            <i class="fas fa-times"></i>
+		        </div>
+            </div>
+        </button>
         <div class="container flex-center">
             <div class="list mini">
                 <button class="tab-button transform-button round selected"  data-id="change-password" data-title="Change Password">
@@ -56,31 +65,24 @@ checkEmployeeStatus(); // Check if the employee is signed in
 		    </div>
             <div class="list sub">
                 <h2 id="title">Change Password</h2>
-                <form id="change-password">
-                    <label for="current-password" class="info">Current Password</label>
-    	            <div class="form-item">
-    		            <input id="current-password" class="input-field" type="password">
-    	            </div>
-    	            <hr>
+                <form id="change-password" action="../../../requests/workspace/user/updatePassword" class="flex-form">
+    	            <label for="old-password" class="info">Current Password</label>
+    		        <input id="old-password" type="password" name="old" class="input-field" required>
+    		        <hr>
     	            <label for="new-password" class="info">New Password</label>
-    	            <div class="form-item">
-    		            <input id="new-password" class="input-field" type="password">
-    	            </div>
+    		        <input id="new-password" type="password" name="new" class="input-field" required>
     	            <label for="confirm-password" class="info">Confirm Password</label>
-    	            <div class="form-item">
-    		            <input id="confirm-password" class="input-field" type="password">
-    	            </div>
-                    <hr>
-                    <div class="form-item">
-                        <button form="change-password" class="standard-button transform-button flex-center round">
-                            <div class="split">
-                                <p class="animate-left">Apply<p>
-               		            <div class="toggle-button">
-                		            <i class="fas fa-chevron-right"></i>
-                		        </div>
-                            </div>
-                        </button>
-                    </div>
+    		        <input id="confirm-password" type="password" name="confirm" class="input-field" required>
+                    <input type="hidden" name="token" value="<?php echo $passwordToken ?>">
+                    <input type="hidden" name="id" value="<?php echo $_SESSION['uid'] ?>">
+                    <button type="submit" class="standard-button transform-button flex-center round">
+                        <div class="split">
+                            <p class="animate-left">Apply<p>
+           		            <div class="toggle-button">
+            		            <i class="fas fa-chevron-right"></i>
+            		        </div>
+                        </div>
+                    </button>
                 </form>
             </div>
             <div class="list mini">
@@ -89,5 +91,35 @@ checkEmployeeStatus(); // Check if the employee is signed in
     	</div>
 	</body>
 	<script type="text/javascript" src="../../../js/navigation.js"></script>
+	<script type="text/javascript" src="../../../js/notification.js"></script>
+	<script type="text/javascript" src="../../../js/post.js"></script>
 	<script type="text/javascript" src="../../../js/tabs.js"></script>
+	<script type="text/javascript">
+	    document.getElementById('change-password').addEventListener('submit', async (event) => {
+	        event.preventDefault();
+	        
+            let form = event.target;
+            let formData = new FormData(form);
+            
+            if (formData.get('new') === formData.get('confirm')) {
+                let json = await getJson(form.action, formData);
+                
+                if (!isEmptyJson()) {
+                    if (json.response) {
+                        setSuccessNotification(json.message);
+                        form.reset();
+                    } else {
+                        setFailNotification(json.message);
+                    }
+                } else {
+                    setFailNotification("Failed to update password");
+                }
+            } else {
+                document.getElementById('new-password').focus();
+                setFailNotification('Passwords Do Not Match');
+            }
+            
+            showNotification();
+	    });
+	</script>
 </html>
