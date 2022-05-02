2f504643f7a2843ebdf94f54ebfca8b0ca621d71
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-05-02 18:31:59 -0400

    Rename error.php to index.php

--- public_html/goldmanstacks/view/error/error.php
+++
@@ -1,2 +0,0 @@
-<?
-header('Location: https://' . $_SERVER['HTTP_HOST'] . '/' . strtok($_SERVER["REQUEST_URI"], '/') . '/goldmanstacks/view/signin.php?error=' . $_GET['code']); // Redirect to sign in page
