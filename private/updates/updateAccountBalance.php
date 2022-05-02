32f75df879e9194072cadc17b48adad51213d34d
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-05-02 13:11:58 -0400

    Create updateAccountBalance.php

---
+++ private/updates/updateAccountBalance.php
@@ -0,0 +1,15 @@
+<?php
+require_once('../config.php');
+
+$db = $getUpdateConnection();
+
+if ($db !== null) {
+    /* Update balance of all savings accounts using interest rate */
+    $updateBalance = $db->prepare("UPDATE accountDirectory A INNER JOIN savings S ON A.accountNum=S.accountNum SET A.balance=A.balance*(S.interestRate/100+1)");
+    
+    if ($db->affected_rows === 0) {
+        /* Write log file */
+    }
+    
+    $db->close();
+}
