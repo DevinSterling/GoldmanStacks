29d24f6c7abb0506d1a385d8a3326249fb7a9a19
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-05-09 11:43:28 -0400

    Included more details

--- public_html/goldmanstacks/view/account/statement.php
+++ public_html/goldmanstacks/view/account/statement.php
@@ -72,66 +72,121 @@ if ($db === null) {
                     </button>
 		        </div>
 		        <div id="Statement">
-                <table id="transactions" class="responsive-table">
-                    <thead>
-	                    <tr>
-	                        <th class="date">Date</th>
-	                        <th class="desc">Description</th>
-	                        <th class="amount text-right">Amount</th>
-	                        <th class="hidden">Balance After</th>
-	                        <th class="hidden">Type</th>
-	                    </tr>
-                    </thead>
-                    <tbody tabindex="0" id="transactions-body">
+	            <p class="info">Details<p>
 		            <?php
-                    /* Query to get all transactions from the selected account */
-                    $transactionStatement = $db->prepare("SELECT T.transactionTime, T.accountNum, T.recipientAccount, T.transactionAmount, T.type, A.nickName, A.accountType, (T.recipientAccount=A.accountNum AND T.clientID<>A.clientID) AS isRecipient
-                                                                FROM transactions T
-                                                                INNER JOIN accountDirectory A ON T.accountNum=A.accountNum OR (T.recipientAccount=A.accountNum AND T.clientID<>A.clientID)
-                                                                WHERE A.clientID=?
-                                                                ORDER BY T.transactionTime DESC");
-                    $transactionStatement->bind_param("s", $userId);
-                    $transactionStatement->execute();
-                    
-                    /* Obtain result */
-                    $result = $transactionStatement->get_result();
-                    $rows = $result->fetch_all(MYSQLI_ASSOC);
-
-                    foreach ($rows as $transaction) {
-                        switch ($transaction['type']) {
-                            case 'transfer':
-                            case 'payment':
-                                if ($transaction['isRecipient']) {
-                                    $description = ucfirst($transaction['type']) . " from (*" . substr($transaction['accountNum'], -4) . ")";
-                                    $transaction['transactionAmount'] *= -1;
-                                } else {
-                                    $description = ucfirst($transaction['type']) . " to (*" . substr($transaction['recipientAccount'], -4) . ")";
-                                }
-                                break;
-                            case 'deposit':
-                                $description = "Deposit into account";
-                                break;
-                            case 'withdraw':
-                                $description = "Withdraw from account";
-                                break;
+		                $clientStatement = $db->prepare("SELECT firstName, middleName, lastName, email, phoneNumber, line1, line2, city, state, postalCode FROM users U INNER JOIN address A ON A.userID=U.userID WHERE U.userID=?");
+		                $clientStatement->bind_param("i", $userId);
+		                $clientStatement->execute();
+		                
+                        $result = $clientStatement->get_result();
+                        $details = $result->fetch_assoc();
+                        
+                        $clientStatement->close();
+		            ?>
+		            <div class="split margin-bottom">
+		                <div>
+        		            <p class="info"><b>Name:</b> <?php echo ucfirst($details['firstName']) . " " . ucfirst($details['middleName']) . " " . ucfirst($details['lastName']) ?></p>
+        		            <p class="info"><b>Email:</b> <?php echo $details['email'] ?></p>
+                            <p class="info"><b>Phone:</b> <?php echo convertToPhoneNumber($details['phoneNumber']) ?></p>
+    		            </div>
+    		            <div class="text-left">
+    		                <p class="info"><b>Address</b></p>
+        		            <p class="info"><?php echo $details['line1'] ?></p>
+                            <?php
+        			        if (!empty($details['line2'])) echo "<p class=\"info\"><b>Line 2:</b>" . ($details['line2']) . "</p>";
+                            ?>
+        		            <p class="info"><?php echo $details['city'] . ", " . $details['state'] ?>, </p>
+                            <p class="info"><?php echo $details['postalCode'] ?></p>
+    		            </div>
+		            </div>
+                    <table id="accounts" class="responsive-table margin-bottom">
+                        <thead>
+    	                    <tr>
+    	                        <th class="date">Account</th>
+    	                        <th class="desc">Type</th>
+    	                        <th class="desc">Balance</th>
+    	                    </tr>
+                        </thead>
+                        <tbody>
+    		            <?php
+    		                $accountStatement = $db->prepare("SELECT nickName, accountNum, accountType, balance FROM accountDirectory WHERE clientID=?");
+    		                $accountStatement->bind_param("i", $userId);
+    		                $accountStatement->execute();
+    		                
+                            $result = $accountStatement->get_result();
+                            $accounts = $result->fetch_all(MYSQLI_ASSOC);
+    		                
+    		                foreach ($accounts as $account) {
+    		                    echo "<tr>
+        		                        <td data-label=\"Account\">" . ucfirst($account['nickName']) . " (*" . substr($account['accountNum'], -4) . ")</td>
+        		                        <td data-label=\"Type\">" . ucfirst($account['accountType']) . "</td>
+        		                        <td data-label=\"Balance\">\$" . number_format($account['balance'], 2) . "</td>
+    		                        </tr>";
+    		                }
+    		                
+    		                $accountStatement->close();
+    		            ?>
+    		            </tbody>
+		            </table>
+		            <p class="info">Transactions<p>
+                    <table id="transactions" class="responsive-table">
+                        <thead>
+    	                    <tr>
+    	                        <th class="date">Date</th>
+    	                        <th class="desc">Account</th>
+    	                        <th class="desc">Description</th>
+    	                        <th class="amount text-right">Amount</th>
+    	                    </tr>
+                        </thead>
+                        <tbody>
+    		            <?php
+                        /* Query to get all transactions from the selected account */
+                        $transactionStatement = $db->prepare("SELECT T.transactionTime, T.accountNum, T.recipientAccount, T.transactionAmount, T.type, A.nickName, A.accountType, (T.recipientAccount=A.accountNum AND T.clientID<>A.clientID) AS isRecipient
+                                                                    FROM transactions T
+                                                                    INNER JOIN accountDirectory A ON T.accountNum=A.accountNum OR (T.recipientAccount=A.accountNum AND T.clientID<>A.clientID)
+                                                                    WHERE A.clientID=?
+                                                                    ORDER BY T.transactionTime DESC");
+                        $transactionStatement->bind_param("s", $userId);
+                        $transactionStatement->execute();
+                        
+                        /* Obtain result */
+                        $result = $transactionStatement->get_result();
+                        $rows = $result->fetch_all(MYSQLI_ASSOC);
+    
+                        foreach ($rows as $transaction) {
+                            switch ($transaction['type']) {
+                                case 'transfer':
+                                case 'payment':
+                                    if ($transaction['isRecipient']) {
+                                        $description = ucfirst($transaction['type']) . " from (*" . substr($transaction['accountNum'], -4) . ")";
+                                        $transaction['transactionAmount'] *= -1;
+                                    } else {
+                                        $description = ucfirst($transaction['type']) . " to (*" . substr($transaction['recipientAccount'], -4) . ")";
+                                    }
+                                    break;
+                                case 'deposit':
+                                    $description = "Deposit into account";
+                                    break;
+                                case 'withdraw':
+                                    $description = "Withdraw from account";
+                                    break;
+                            }
+                            
+                            
+                            echo "<tr class=\"transaction-element\">
+                                <td data-label=\"Date\" class=\"date\">".$transaction['transactionTime']."</td>
+                                <td data-label=\"Account\" class=\"date\">" . ucfirst($transaction['nickName']) . " (*".substr($transaction['accountNum'], -4).")</td>
+                                <td data-label=\"Description\" class=\"desc\">$description</td>
+                                <td data-label=\"Amount\" class=\"amount text-right\">".convertToCurrency($transaction['transactionAmount'])."</td>
+                            </tr>";
                         }
                         
-                        
-                        echo "<tr class=\"transaction-element\">
-                            <td data-label=\"Balance After\" class=\"hidden\">\$1000.00</td>
-                            <td data-label=\"Type\" class=\"hidden\">".ucfirst($transaction['type'])."</td>
-                            <td data-label=\"Date\" class=\"date\">".$transaction['transactionTime']."</td>
-                            <td data-label=\"Description\" class=\"desc\">$description</td>
-                            <td data-label=\"Amount\" class=\"amount text-right\">".convertToCurrency($transaction['transactionAmount'])."</td>
-                        </tr>";
-                    }
-                    
-                    $result->free();
-                    $transactionStatement->close();
-                    $db->close();
-		            ?>
-		            </tbody>
-	            </table>
+                        $result->free();
+                        $transactionStatement->close();
+                        $db->close();
+    		            ?>
+    		            </tbody>
+    	            </table>
 		        </div>
 		    </div>
 		</div>
