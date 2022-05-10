2c9df8d3d898eb36f1e6b7724bbce67725a18371
Devin <84195485+DevinSterling@users.noreply.github.com>
2022-05-10 13:15:58 -0400

    Switched variables to constants

--- public_html/goldmanstacks/js/print.js
+++ public_html/goldmanstacks/js/print.js
@@ -1,6 +1,6 @@
 function printSelected(elementId) {
-    var page = document.body.innerHTML;
-    var content = document.getElementById(elementId).innerHTML;
+    const page = document.body.innerHTML;
+    const content = document.getElementById(elementId).innerHTML;
     
     // Selected content only when printing
     document.body.innerHTML = content;
